6b57e7fa18b5ee9fbab069ccee5b56cb41d26f63
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-03-10 19:19:02 +0530

    added: exact 100 logic

--- Main.java
+++ Main.java
@@ -6,17 +6,27 @@ public class Main {
     public static void main(String[] args) {
         int player = 0; int count = 0;
         int stay = 0; int ladder = 1; int snake = 2;
-       while (player <= 100) {
+       while (player != 100) {
            int die = (int) (Math.floor(Math.random() * 10) % 6) + 1;
            int choice = (int) Math.floor(Math.random() * 10) % 3;
-                if (choice == 0) {
-                     player = player + 0;
-                 } else if (choice == 1) {
-                     player = player + die;
-                } else if (choice == 2) {
-                    player = player - die;
-                }
-          count++;
+           if (player>=0 && player<100) {
+               if (choice == 0) {
+                   player = player + 0;
+               }
+               else if (choice == 1) {
+                   player = player + die;
+               }
+               else if (choice == 2) {
+                   player = player - die;
+               }
+           }
+           else if (player < 0){
+               player = 0;
+           }
+           else if (player >= 100){
+               player = player - die;
+           }
+                count++;
        }
         System.out.println("game over : " + player);
         System.out.println("count " + count);
