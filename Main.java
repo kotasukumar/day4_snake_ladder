fb45fc01d3c9b29c82611ee960838546643d534e
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-03-10 19:22:57 +0530

    added: final display logic

--- Main.java
+++ Main.java
@@ -5,26 +5,32 @@ public class Main {
 
     public static void main(String[] args) {
         int player = 0; int count = 0;
-        int stay = 0; int ladder = 1; int snake = 2;
+        int stay = 0, ladder = 1, snake = 2;
        while (player != 100) {
            int die = (int) (Math.floor(Math.random() * 10) % 6) + 1;
            int choice = (int) Math.floor(Math.random() * 10) % 3;
            if (player>=0 && player<100) {
-               if (choice == 0) {
+               if (choice == stay) {
                    player = player + 0;
+                   System.out.println("position: " + player);
                }
-               else if (choice == 1) {
+               else if (choice == ladder) {
                    player = player + die;
+                   System.out.println("position: " + player);
                }
-               else if (choice == 2) {
+               else if (choice == snake) {
                    player = player - die;
+                   System.out.println("position: " + player);
                }
            }
            else if (player < 0){
                player = 0;
+               System.out.println("game is restarted");
+               System.out.println("position: " + player);
            }
            else if (player >= 100){
                player = player - die;
+               System.out.println("position: " + player);
            }
                 count++;
        }
