6576b1b20e506e33c006b463dd042b597b0d8d99
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-03-10 18:11:59 +0530

    added:untill 100 logic

--- Main.java
+++ Main.java
@@ -4,19 +4,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int player = 0;
-    int die = (int) (Math.floor(Math.random() * 10) % 6) + 1;
-    int choice = (int) Math.floor(Math.random() * 10) %3;
-    int stay = 0, ladder = 1, snake = 2;
-        System.out.println("die ; " + die);
-        if (choice == stay){
-            System.out.println("stay at same position");
-        }
-        else if (choice == ladder){
-            System.out.println("move forward");
-        }
-        else if (choice == snake){
-            System.out.println("come back");
-        }
+        int player = 0; int count = 0;
+        int stay = 0; int ladder = 1; int snake = 2;
+       while (player <= 100) {
+           int die = (int) (Math.floor(Math.random() * 10) % 6) + 1;
+           int choice = (int) Math.floor(Math.random() * 10) % 3;
+                if (choice == 0) {
+                     player = player + 0;
+                 } else if (choice == 1) {
+                     player = player + die;
+                } else if (choice == 2) {
+                    player = player - die;
+                }
+          count++;
+       }
+        System.out.println("game over : " + player);
+        System.out.println("count " + count);
     }
 }
+
+
